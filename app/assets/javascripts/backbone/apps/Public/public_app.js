53fe47f7ce4830aa29d2eb2df1969b92c387cf00
BenoitLecolier <ben.lecolier.pro@gmx.com>
2014-05-07 15:47:54 +0200

    Added public router and controller

--- app/assets/javascripts/backbone/apps/Public/public_app.js
+++ app/assets/javascripts/backbone/apps/Public/public_app.js
@@ -1,3 +1,14 @@
 PocApp.module("PublicApp", function (PublicApp, App, Backbone, Marionette, $, _) {
 
+    PublicApp.Router = Marionette.AppRouter.extend({
+        appRoutes: {
+            'public': 'showPublic'
+        }
+    });
+
+    var API = {showPublic: function(){return PublicApp.Show.Controller.showPublic();}};
+
+    PocApp.addInitializer(function(){
+        return new PublicApp.Router({ controller: API });
+    });
 });
--- app/assets/javascripts/backbone/apps/Public/show/show_controller.js
+++ app/assets/javascripts/backbone/apps/Public/show/show_controller.js
@@ -1,6 +1,8 @@
 PocApp.module("PublicApp.Show", function (Show, App, Backbone, Marionette, $, _) {
 
     Show.Controller = {
-
+        showPublic: function() {
+            console.log("public controller");
+        }
     };
 });
